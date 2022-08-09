1c082313513a4c29ed2990d7cd819eab23e1cf56
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-09 14:20:48 +0300

    Make changes to main.js and catalog.js so that script of catalog can run properly

--- out-of-this-world/public/js/main.js
+++ out-of-this-world/public/js/main.js
@@ -13,43 +13,67 @@
 // });
 
 //navigation bar effects on scroll
-window.addEventListener('load', ()=>{ 
-    window.addEventListener("scroll", function () {
-        const header = document.querySelector("header");
-        header.classList.toggle("sticky", window.scrollY > 0);
-    });
-    
-    //responsive navigation menu
-    // const menuBtn = document.querySelector(".nav-menu-btn");
-    // const closeBtn = document.querySelector(".nav-close-btn");
-    const navigation = document.querySelector(".navigation");
-    
-    // menuBtn.addEventListener("click", () => {
-    //     navigation.classList.add("active");
-    // });
-    // closeBtn.addEventListener("click", () => {
-    //     navigation.classList.remove("active");
-    // });
+window.addEventListener('load', () => {
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
 
-    const signUpBtn = document.querySelector(".sign-up-btn");
-    const signInBtn = document.querySelector(".sign-in-btn");
-    const signUpForm = document.querySelector(".sign-up-form");
-    const signInForm = document.querySelector(".sign-in-form");
+  //responsive navigation menu
+  // const menuBtn = document.querySelector(".nav-menu-btn");
+  // const closeBtn = document.querySelector(".nav-close-btn");
+  // const navigation = document.querySelector(".navigation");
 
+  // menuBtn.addEventListener("click", () => {
+  //     navigation.classList.add("active");
+  // });
+  // closeBtn.addEventListener("click", () => {
+  //     navigation.classList.remove("active");
+  // });
+
+  const signUpBtn = document.querySelector(".sign-up-btn");
+  const signInBtn = document.querySelector(".sign-in-btn");
+  const signUpForm = document.querySelector(".sign-up-form");
+  const signInForm = document.querySelector(".sign-in-form");
+  if(signUpBtn && signInBtn){
     signUpBtn.addEventListener("click", () => {
       signUpForm.classList.add("show");
       signInForm.classList.add("hide");
       signUpForm.classList.remove("hide");
       signInForm.classList.remove("show");
-
+  
     });
-
+  
     signInBtn.addEventListener("click", () => {
       signInForm.classList.add("show");
       signUpForm.classList.add("hide");
       signInForm.classList.remove("hide");
       signUpForm.classList.remove("show");
     });
+  }
+
+
+  // Catalog js
+
+  let moon = document.querySelector(".moon");
+  let star = document.querySelector(".star")
+  let title = document.querySelector("#title");
+  
+  console.log(moon, star);
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+
+    star.style.left = value * 0.2 + "px";
+    moon.style.top = 50 + value * 1.05 + "px";
+
+
+    title.style.marginRight = value * 5 + "px";
+    title.style.top = 300 + value * 0.9 + "px";
+
+
+
+  });
+
 });
 
 
--- out-of-this-world/src/components/Catalog.js
+++ out-of-this-world/src/components/Catalog.js
@@ -5,7 +5,7 @@ const Catalog = () => {
     return (
         <div id="catalog">
             <section id="catalog">
-                <img src="../images/background-catalog/stars.png" className="stars" alt="" />
+                <img src="../images/background-catalog/stars.png" className="star" alt="" />
                 <img src="../images/background-catalog/moon.png" className="moon" alt="" />
                 <h2 id="title">Catalog</h2>
             </section>
@@ -18,20 +18,33 @@ const Catalog = () => {
 
 
                 <div id="top">
-
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>
                 <h2 className="flating-title">All</h2>
 
                 <div id="all">
                     <img src="/images/wavebottom.png" alt="" className="bottom-img" />
                     <img src="/images/wavetop.png" alt="" className="top-img" />
-
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>
 
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+
 
             </section>
         </div>
