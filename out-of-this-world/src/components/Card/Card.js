4e4856ee1ea347f9ed4032ce0611d0f79f807229
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-09 11:06:27 +0300

    Add Card component

---
+++ out-of-this-world/src/components/Card/Card.js
@@ -0,0 +1,23 @@
+import './Card.css';
+
+const Card = () => {
+
+    return (
+        <div class="card">
+                <img src="/images/home-page/2nd.jpg" alt=""/>
+
+                <div class="card-wrapper">
+                    <h3>Title</h3>
+                    <span class="year">2016</span>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium nostrum, neque cum
+                        provident
+                        distinctio quibusdam fugit! Sed officiis blanditiis iusto commodi quasi illum!Lorem ipsum dolor
+                    </p>
+                    <button class="details btn">See more</button>
+                </div>
+
+            </div>
+    );
+}
+
+export default Card;
