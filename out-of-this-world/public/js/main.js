8d1186e6d797f521fb44d207ea8ab5137eb0b0a6
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-09 12:45:41 +0300

    Move the js folder to the public folder

--- out-of-this-world/public/js/main.js
+++ out-of-this-world/public/js/main.js
@@ -1,16 +1,16 @@
 // swiper
-var swiper = new Swiper(".bg-slider-thumbs", {
-    loop: true,
-    spaceBetween: 0,
-    slidesPerView: 0
-});
-var swiper2 = new Swiper(".bg-slider", {
-    loop: true,
-    spaceBetween: 0,
-    thumbs: {
-        swiper: swiper,
-    },
-});
+// var swiper = new Swiper(".bg-slider-thumbs", {
+//     loop: true,
+//     spaceBetween: 0,
+//     slidesPerView: 0
+// });
+// var swiper2 = new Swiper(".bg-slider", {
+//     loop: true,
+//     spaceBetween: 0,
+//     thumbs: {
+//         swiper: swiper,
+//     },
+// });
 
 //navigation bar effects on scroll
 
