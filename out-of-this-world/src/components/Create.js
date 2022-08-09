ad0e5cbfe96ddbca49b0e0f1331e0db7d6d78e8f
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-09 13:24:56 +0300

    Add create and details

---
+++ out-of-this-world/src/components/Create.js
@@ -0,0 +1,52 @@
+const Create = () => {
+
+    return (
+        <div className="hero create">
+
+            <video autoplay loop muted plays-inline>
+                <source src="/images/video3.mp4" type="video/mp4" />
+            </video>
+
+
+            <div className="form-box">
+                <h2>ADD A FILM</h2>
+
+                <form action="">
+                    <div className="left">
+                        <div className="field float-left">
+                            <i className="uil uil-circle"></i>
+                            <input type="text" placeholder="Title" required />
+                        </div>
+
+                        <div className="field">
+                            <i className="uil uil-rocket"></i>
+                            <input type="number" placeholder="Year" required />
+                        </div>
+                        <div className="field float-right radio">
+                            <i className="uil uil-star"></i>
+                            <p>Public: </p>
+                            <input type="radio" id="yes" name="radio" value="yes" />
+                            <label htmlFor="yes">Yes</label><br />
+                            <input type="radio" id="no" name="radio" value="no" />
+                            <label htmlFor="no">No</label><br />
+                        </div>
+
+                    </div>
+                    <div className="right">
+                        <div className="field ">
+                            <i className="uil uil-align-justify"></i>
+                            <textarea type="text" placeholder="Description"></textarea>
+                        </div>
+
+                        <input type="submit" className="submit-btn" value="Create" />
+                    </div>
+
+
+                </form>
+            </div>
+
+        </div>
+    );
+}
+
+export default Create;
---
+++ out-of-this-world/src/components/Details.js
@@ -0,0 +1,64 @@
+const Details = () => {
+
+    return (
+        <div class="hero details">
+
+        <video autoplay loop muted plays-inline>
+            <source src="/images/video3.mp4" type="video/mp4" />
+        </video>
+
+        <div class="content">
+            <div class="left">
+                <div class="images">
+                    <img src="../images/home-page/1st.jpg" alt=""/>
+                    <img src="../images/home-page/2nd.jpg" alt=""/>
+                </div>
+
+
+                <div class="wrapper">
+                    <h3>Title</h3>
+                    <b>11</b>
+                    <span>2013</span>
+                    <div class="buttons">
+                        <button>Like</button>
+                        <button>Dislike</button>
+
+                         {/* <button>Edit</button>
+                        <button>Delete</button>  */}
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="right">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maxime vel assumenda consequatur
+                    accusantium fugit, quibusdam alias facilis dolorum reprehenderit eius deserunt at. Eum eveniet
+                    aliquam quos deserunt perspiciatis modi?
+                    A similique tenetur quibusdam dicta alias odit officia accusamus esse quis pariatur delectus, saepe
+                    iusto consequuntur at placeat nisi, adipisci, sint ut aut incidunt quasi est. Temporibus dignissimos
+                    fugit rerum!
+                    Non culpa quibusdam recusandae at quisquam distinctio hic, eius, porro velit optio nisi assumenda,
+                    beatae perferendis ratione? Harum, aliquam quis possimus nostrum saepe aperiam, officia tenetur
+                    tempora ad qui quisquam.
+                    Reprehenderit nihil delectus beatae omnis, nostrum optio ut doloribus suscipit repellendus tenetur
+                    modi quos voluptas aspernatur hic non. Aliquid, minus sapiente. Sit natus quibusdam inventore, dicta
+                    autem cupiditate culpa sunt.
+                    Sequi omnis eaque numquam fugit, doloremque reiciendis ullam exercitationem dolor quibusdam debitis
+                    iure temporibus fugiat iste distinctio soluta natus voluptatum rerum tempora beatae sed officiis
+                    vitae explicabo, quasi hic? Esse!
+                    Repellendus at, alias quos dolorem ullam eius fugiat modi fuga, libero aut ipsam dicta saepe laborum
+                    voluptatibus deserunt sapiente architecto pariatur. Atque earum corrupti sit consequatur dolorem hic
+                    qui numquam?
+                    Perspiciatis maxime, nisi amet sapiente officiis repellendus, placeat ad consequuntur reprehenderit
+                    ipsum numquam saepe labore quos optio sint vero tempora ut! Doloremque, rerum magnam facilis odit
+                    laborum nesciunt saepe sit!
+                    Laborum corporis incsapiente nisi impedit nostrum molestiae? Iste impedit nostrum molestiae? Iste.</p>
+            </div>
+        </div>
+
+
+    </div>
+    );
+}
+
+export default Details;
