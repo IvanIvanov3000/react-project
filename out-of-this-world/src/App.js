541e08eb66904c5416fbf81d2611ae1e69099b44
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-09 11:53:46 +0300

    Add a git ignore due to the node modules being constantly uploaded

--- out-of-this-world/src/App.js
+++ out-of-this-world/src/App.js
@@ -2,6 +2,8 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Catalog from './components/Catalog/Catalog';
+
 // import GameCatalog from './components/GameCatalog/GameCatalog';
 // import CreateGame from './components/CreateGame';
 // import Login from './components/Login';
@@ -17,8 +19,8 @@ function App() {
             <main id="main-content">
                 <Switch>
                     <Route path="/home" exact component={Home} />
-                     {/*<Route path="/games" exact component={GameCatalog} />
-                    <Route path="/create-game" component={CreateGame} />
+                    <Route path="/catalog" exact component={Catalog} />
+                    {/*<Route path="/create-game" component={CreateGame} />
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
                     <Route path="/games/:gameId" component={GameDetails} />
