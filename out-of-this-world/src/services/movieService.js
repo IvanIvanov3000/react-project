7f9bd73dbc04361b601a0488f10aebe857f0dacb
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-11 12:31:06 +0300

    Add create service

--- out-of-this-world/src/services/movieService.js
+++ out-of-this-world/src/services/movieService.js
@@ -10,20 +10,8 @@ export const getAll = () => request.get(`${baseUrl}catalog`);
 //     return request.get(`${baseUrl}/pets?where=${query}`);
 // };
 
-// export const create = async (petData, token) => {
-//     let response = await fetch(`${baseUrl}/pets`, {
-//         method: 'POST',
-//         headers: {
-//             'content-type': 'application/json',
-//             'X-Authorization': token,
-//         },
-//         body: JSON.stringify({...petData, likes: []})
-//     });
+export const create = (movieData) => request.post(`${baseUrl}create`, movieData);
 
-//     let result = await response.json();
-
-//     return result;
-// };
 
 // export const update = (petId, petData) => request.put(`${baseUrl}/pets/${petId}`, petData);
 
