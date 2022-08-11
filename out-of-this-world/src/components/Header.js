8c991f5b5be4bcf9567a78a991ac611cbecd3500
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-11 10:21:58 +0300

    Change header if the person is logged in or if not

--- out-of-this-world/src/components/Header.js
+++ out-of-this-world/src/components/Header.js
@@ -2,18 +2,40 @@ import React from 'react';
 import { FaHome, FaSearch, FaEnvelope, FaRegStar } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
+import { useAuthContext } from '../contexts/AuthContext';
+
 
 const Header = () => {
+    const { user } = useAuthContext();
+
+    let guestNavigation = (
+        <>
+            <Link to="/login"><FaEnvelope className='icon' /> Login</Link>
+        </>
+    );
+
+    let userNavigation = (
+        <>
+            <Link to="/my-pets"><FaEnvelope className='icon' /> My Movies</Link>
+            <Link to="/create"><FaEnvelope className='icon' /> Create Movie</Link>
+            <Link to="/logout"><FaEnvelope className='icon' /> Logout</Link>
+        </>
+    );
+
     return (
         <header>
             <div className="nav-bar">
                 <Link to='/home'><img src="/images/promaniq22.png" alt="logo" /></Link>
                 <div className="navigation">
                     <div className="nav-items">
-                        <Link to="/home"><FaHome className='icon'/> Home</Link>
-                        <Link to="/catalog"><FaSearch className='icon'/> Explore</Link>
-                        <Link to="/blog"><FaRegStar className='icon'/> Blog</Link>
-                        <Link to="/login"><FaEnvelope className='icon'/> Login</Link>
+                        <Link to="/home"><FaHome className='icon' /> Home</Link>
+                        <Link to="/catalog"><FaSearch className='icon' /> Explore</Link>
+                        <Link to="/blog"><FaRegStar className='icon' /> Blog</Link>
+                        
+                        {user.email
+                            ? userNavigation
+                            : guestNavigation
+                        }
                     </div>
                 </div>
             </div>
