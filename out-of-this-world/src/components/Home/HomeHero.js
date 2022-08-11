59ea1322d2580ea7f598e2cd1a397681dd9d4daf
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-11 21:01:12 +0300

    Change the way of rendering the files in HomeHero by saving them in a local js file as array

--- out-of-this-world/src/components/Home/HomeHero.js
+++ out-of-this-world/src/components/Home/HomeHero.js
@@ -6,108 +6,50 @@ import React, { useState } from 'react';
 
 import 'swiper/css';
 
-
+import movies from './topMovies.js';
 
 SwiperCore.use([Navigation, Pagination, Controller, Thumbs]);
 
 const HomeHero = () => {
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
-
+    console.log(movies);
     const slides = [];
+    for (let i = 0; i < movies.length; i++) {
+        slides.push(
+            <SwiperSlide key={`slide-${i}`} tag="div" className="swiper-wrapper">
+                <div className="swiper-slide">
+                    <img src={movies[i].image} alt="" />
+                    <div className="text-content">
+                        <h2 className="title">{movies[i].title} <span>{movies[i].year}</span></h2>
+                        <p>{movies[i].description}</p>
+                        <a style={{ textDecoration: "none" }} href={movies[i].link}><button className="read-btn">Read More <FaArrowRight className='icon' /></button></a>
+                    </div>
+                </div>
+            </SwiperSlide>
+        )
 
-    slides.push(
-        <SwiperSlide key={`slide-${1}`} tag="div" className="swiper-wrapper">
-            <div className="swiper-slide">
-                <img src="/images/home-page/1st.jpg" alt="" />
-                <div className="text-content">
-                    <h2 className="title">Interstellar <span>2014</span></h2>
-                    <p>Interstellar is about Earth's last chance to find a habitable planet before a lack of
-                        resources causes the human race to go extinct. The film's protagonist is Cooper (Matthew
-                        McConaughey), a former NASA pilot who is tasked with leading a mission through a wormhole to
-                        find a habitable planet in another galaxy.
-                    </p>
-                    <button className="read-btn">Read More <FaArrowRight className='icon' /></button>
-                </div>
-            </div>
-        </SwiperSlide>,
-        <SwiperSlide key={`slide-${2}`} tag="div" className="swiper-wrapper">
-            <div className="swiper-slide">
-                <img src="/images/home-page/2nd.jpg" alt="" />
-                <div className="text-content">
-                    <h2 className="title">The Martian <span>2015</span></h2>
-                    <p>During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm
-                        and left behind by his crew. But Watney has survived and finds himself stranded and alone on
-                        the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and
-                        spirit to subsist and find a way to signal to Earth that he is alive.
-                    </p>
-                    <button className="read-btn">Read More <FaArrowRight className='icon' /></button>
-                </div>
-            </div>
-        </SwiperSlide>,
-        <SwiperSlide key={`slide-${3}`} tag="div" className="swiper-wrapper">
-            <div className="swiper-slide dark-layer">
-                <img src="/images/home-page/3rd.jpg" alt="" />
-                <div className="text-content">
-                    <h2 className="title">Hidden figures <span>2016</span></h2>
-                    <p>As the United States raced against Russia to put a man in space, NASA found untapped talent
-                        in a group of African-American female mathematicians that served as the brains behind one of
-                        the greatest operations in U.S. history. Based on the unbelievably true life stories of
-                        three of these women, known as "human computers", we follow these women as they quickly rose
-                        the ranks of NASA alongside many of history's greatest minds specifically tasked with
-                        calculating the momentous launch of astronaut John Glenn into orbit, and guaranteeing his
-                        safe return.
-                    </p>
-                    <button className="read-btn">Read More <FaArrowRight className='icon' /></button>
-                </div>
-            </div>
-        </SwiperSlide>,
-        <SwiperSlide key={`slide-${4}`} tag="div" className="swiper-wrapper">
-            <div className="swiper-slide dark-layer">
-                <img src="/images/home-page/4th.jpg" alt="" />
-                <div className="text-content">
-                    <h2 className="title">Passagers <span>2016</span></h2>
-                    <p>The spaceship, Starship Avalon, in its 120-year voyage to a distant colony planet known as
-                        the "Homestead Colony" and transporting 5,258 people has a malfunction in one of its sleep
-                        chambers. As a result one hibernation pod opens prematurely and the one person that awakes,
-                        Jim Preston (Chris Pratt) is stranded on the spaceship, still 90 years from his destination
-                    </p>
-                    <button className="read-btn">Read More <FaArrowRight className='icon' /></button>
-                </div>
-            </div>
-        </SwiperSlide>
-
-    );
-
+    }
     const thumbs = [];
 
-    thumbs.push(
-        <SwiperSlide key='1' style={{ backgroundImage: `url("/images/home-page/1st.jpg")` }}>
+    for (let i = 0; i < movies.length; i++) {
+        let image = movies[i].image;
+        thumbs.push(
+            <SwiperSlide key={i} style={{ backgroundImage: `url(${image})`}} />
+        )
 
-        </SwiperSlide>,
+    }
 
-        <SwiperSlide key='2' style={{ backgroundImage: `url("/images/home-page/2nd.jpg")` }}>
 
-        </SwiperSlide>,
-
-        <SwiperSlide key='3' style={{ backgroundImage: `url("/images/home-page/3rd.jpg")` }}>
-
-        </SwiperSlide>,
-
-        <SwiperSlide key='4' style={{ backgroundImage: `url("/images/home-page/4th.jpg")` }}>
-
-        </SwiperSlide>
-
-    );
 
 
 
     return (
         <div id='home-hero'>
             <div className="media-icons">
-                <a href="https://www.facebook.com/"><FaFacebookF className='icon'/></a>
-                <a href="https://www.instagram.com/"><FaInstagram className='icon'/></a>
-                <a href="https://twitter.com/?lang=bg"><FaTwitter className='icon'/></a>
+                <a href="https://www.facebook.com/"><FaFacebookF className='icon' /></a>
+                <a href="https://www.instagram.com/"><FaInstagram className='icon' /></a>
+                <a href="https://twitter.com/?lang=bg"><FaTwitter className='icon' /></a>
 
             </div>
 
---
+++ out-of-this-world/src/components/Home/topMovies.js
@@ -0,0 +1,57 @@
+const movies = [
+
+    {
+
+        title: "Interstellar",
+        year: "2014",
+        image: "/images/home-page/1st.jpg",
+        link: 'https://en.wikipedia.org/wiki/Interstellar_(film)',
+        description: `Interstellar is about Earth's last chance to find a habitable planet before a lack of
+            resources causes the human race to go extinct. The film's protagonist is Cooper (Matthew
+            McConaughey), a former NASA pilot who is tasked with leading a mission through a wormhole to
+            find a habitable planet in another galaxy.`
+
+
+    },
+    {
+
+        title: "The Martian",
+        year: "2015",
+        image: "/images/home-page/2nd.jpg",
+        link: 'https://en.wikipedia.org/wiki/The_Martian_(film)',
+        description: `During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce storm
+            and left behind by his crew. But Watney has survived and finds himself stranded and alone on
+            the hostile planet. With only meager supplies, he must draw upon his ingenuity, wit and
+            spirit to subsist and find a way to signal to Earth that he is alive.`
+
+    },
+    {
+
+        title: "Hidden figures",
+        year: "2016",
+        image: "/images/home-page/3rd.jpg",
+        link: 'https://en.wikipedia.org/wiki/Hidden_Figures',
+        description: `As the United States raced against Russia to put a man in space, NASA found untapped talent
+            in a group of African-American female mathematicians that served as the brains behind one of
+            the greatest operations in U.S. history. Based on the unbelievably true life stories of
+            three of these women, known as "human computers", we follow these women as they quickly rose
+            the ranks of NASA alongside many of history's greatest minds specifically tasked with
+            calculating the momentous launch of astronaut John Glenn into orbit, and guaranteeing his
+            safe return.`
+
+    },
+    {
+        title: "Passagers",
+        year: "2016",
+        image: "/images/home-page/4th.jpg",
+        link: 'https://en.wikipedia.org/wiki/Passengers_(2016_film)',
+        description: `The spaceship, Starship Avalon, in its 120-year voyage to a distant colony planet known as
+            the "Homestead Colony" and transporting 5,258 people has a malfunction in one of its sleep
+            chambers. As a result one hibernation pod opens prematurely and the one person that awakes,
+            Jim Preston (Chris Pratt) is stranded on the spaceship, still 90 years from his destination`
+    }
+
+
+];
+
+export default movies;
