fc01f7730b8a8794968b5a4a604593cb988bf764
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-12 20:35:50 +0300

    Change comment component so that it display the data dynamically not by a static template

--- out-of-this-world/src/components/Blog/Comments.js
+++ out-of-this-world/src/components/Blog/Comments.js
@@ -1,17 +1,9 @@
-const Comment = () => {
+const Comment = ({comment}) => {
 
     return (
         <div className="field">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, repellat ipsam. Saepe, nihil?
-            Vitae est odio veniam exercitationem voluptates minus! Ut voluptates fugiat illum voluptatem
-            doloribus accusantium harum quasi non excepturi architecto, quo, placeat consequatur nihil atque
-            illo hic inventore modi quos id assumenda. Dolore aliquid vero ea, modi dignissimos laborum
-            debitis sit iure iste optio quas voluptates eos corporis cupiditate! Voluptates vero iste atque
-            ipsa voluptate eveniet optio quis dolorem laudantium sapiente at itaque architecto molestias
-            placeat dolor maxime suscipit ea, doloremque recusandae earum cum officia magni? Aspernatur a
-            ape consequatur modi nisi similique est fugit ab? Eveniet ullam voluptate
-            sequi iste expedita eum placeat minus ad libero corporis?</p>
-        <span>Author SecondName</span>
+        <p>{comment.comment}</p>
+        <span>{comment.author.username}</span>
     </div>
     );
 }
