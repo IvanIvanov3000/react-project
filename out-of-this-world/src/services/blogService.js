1565fd1f9f15cc3fc80f33aa9a7ccf97f034f639
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-12 21:20:48 +0300

    Add create comment service

--- out-of-this-world/src/services/blogService.js
+++ out-of-this-world/src/services/blogService.js
@@ -3,4 +3,6 @@ import * as request from './requester';
 const baseUrl = 'http://localhost:3030/blog/';
 
 export const getAll = () => request.get(`${baseUrl}comments`);
-export const getTopMovies = () => request.get(`${baseUrl}comments/top`);
+export const getTopComments = () => request.get(`${baseUrl}comments/top`);
+
+export const createComment= (commentData) => request.post(`${baseUrl}comments`, commentData);
