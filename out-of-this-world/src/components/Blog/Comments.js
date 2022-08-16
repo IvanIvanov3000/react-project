c9442335cfecc319fe4d4680a39d23df18965564
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-16 11:48:09 +0300

    Add a delete Handler to Comments component which makes a delete request to the server and sets the comments in the state

--- out-of-this-world/src/components/Blog/Comments.js
+++ out-of-this-world/src/components/Blog/Comments.js
@@ -1,8 +1,23 @@
 import { useAuthContext } from '../../contexts/AuthContext';
+import * as blogService from '../../services/blogService';
 
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, removeComment}) => {
     const { user } = useAuthContext();
+
+    const deleteHandler = (e) => {
+        e.preventDefault();
+        blogService.deleteComment(comment._id)
+            .then((result) => {
+                removeComment(comment);
+
+            })
+            .catch(err => {
+                // TODO: show notification
+                console.log("error in blog")
+                console.log(err);
+            });
+    }
 
     return (
         <div className="field">
@@ -10,7 +25,7 @@ const Comment = ({ comment }) => {
             <b>Rating : {comment.rating}</b>
             <span>{comment.author.username}</span>
             {user._id === comment.author._id
-                ? <button className="delete btn">Delete</button>
+                ? <button className="delete btn" onClick={deleteHandler}>Delete</button>
                 : <></>
             }
 
