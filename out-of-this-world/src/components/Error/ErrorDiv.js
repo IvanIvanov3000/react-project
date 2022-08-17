a3fc778ef28c25348bd4669f4da690728a03cb8c
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-17 16:11:16 +0300

    Make ErrorDiv component for handeling errors and module css for it

---
+++ out-of-this-world/src/components/Error/ErrorDiv.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+import styles from './ErrorDiv.module.css';
+
+
+
+const ErrorDiv = ({ err }) => {
+
+    return (
+        <>
+            {err.message
+                ? <div className={styles.errorDiv}>
+                    {err.message}
+                </div>
+                : <></>
+            }
+
+        </>
+    )
+}
+
+export default ErrorDiv;
