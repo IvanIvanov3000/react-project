31d583ad511be0f9e36271ad089d5b8623b19b40
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-17 16:13:03 +0300

    Make the error handler return the correct error in server

--- server/Rest-api/utils/errHandler.js
+++ server/Rest-api/utils/errHandler.js
@@ -1,11 +1,16 @@
+const { json } = require("express");
+
 function errorHandler(err, req, res, next) {
-    console.log(req.body);
+    if(err.errors){
+        console.log(Object.values(err.errors)[0].properties.message);
+        return res.status(500).json({message: Object.values(err.errors)[0].properties.message});
+    }
     console.log("error in error handler")
     if (err.status === 333) {
         res.status(333)
             .json({ message: 'ErrorHandler: not allowed!' })
     } else {
-        console.error(err.stack)
+        // console.error(err.stack)
         // console.log(err)
         res.status(500)
             .json({ message: 'ErrorHandler: Something went wrong!', err })
