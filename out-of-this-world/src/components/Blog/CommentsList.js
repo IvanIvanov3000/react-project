6f6096d97e6c46c6a6d12d77e60a476f0dc0711d
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-19 13:21:27 +0300

    Use the no-data style in Blog component in comment list

--- out-of-this-world/src/components/Blog/CommentsList.js
+++ out-of-this-world/src/components/Blog/CommentsList.js
@@ -1,13 +1,13 @@
 import Comment from "./Comments";
 // import { useEffect, useState } from 'react';
-
+import styles from './Blog.module.css'
 
 const CommentsList = ({ comments, functions }) => {
     return (
         <>
             {comments.length > 0
                 ? <>{comments.map(x => <Comment key={x._id} comment={x} functions={functions}/>)}</>
-                : <p className="no-data">No comments in database!</p>
+                : <p className={styles['no-data']}>No comments in database!</p>
             }
         </>
     );
