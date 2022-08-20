dfe30f3bac926b1553b25c51127d65dd902d8884
Ivan <ivan_ivanov_3000@abv.bg>
2022-08-20 17:12:00 +0300

    Chnage the display in home component of the comment of a user who desnt have a photo

--- out-of-this-world/src/components/Home/testimonials/CommentCard.js
+++ out-of-this-world/src/components/Home/testimonials/CommentCard.js
@@ -1,11 +1,12 @@
-import styles from './CommentCard.module.css'
+import styles from './CommentCard.module.css';
+import profile from '../../../assets/images/profile.png'
 const CommentCard = ({ comment }) => {
     return (
         <div className={`col-md-5 ${styles.card}`}>
 
             {comment.author.image
                 ? <div className={`${styles.img} ${styles.circle}`} style={{ backgroundImage: `url(${comment.author.image})` }}></div>
-                : <div className={`${styles.img} ${styles.circle}`} style={{ backgroundImage: `url(/images/profile.png)` }}></div>
+                : <div className={`${styles.img} ${styles.circle}`} ></div>
             }
             <div className={styles.rating}>Rating: {comment.rating}</div>
             <p className={styles["user-details"]}><b>{comment.author.username}</b> <br /> {comment.author.email}</p>
